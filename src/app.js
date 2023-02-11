8788a91f0947c972c6373838d56053397fcbeba0
Abi Turner <turner.abi@gmail.com>
2023-02-11 18:24:36 +0000

    completed string tests

--- src/app.js
+++ src/app.js
@@ -1,10 +1,33 @@
 const express = require('express');
-const { sayHello } = require('./lib/strings');
+const {
+  sayHello,
+  uppercase,
+  lowercase,
+  firstCharacter,
+  firstCharacters,
+} = require('./lib/strings');
 
 const app = express();
 
 app.get('/strings/hello/:string', (req, res) => {
-  res.json({ result: sayHello(req.params.string) });
+  res.status(200).json({ result: sayHello(req.params.string) });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.status(200).json({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.status(200).json({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  res.status(200).json({ result: firstCharacter(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  const n = req.query.length;
+  res.status(200).json({ result: firstCharacters(req.params.string, n) });
 });
 
 module.exports = app;
